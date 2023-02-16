838dfc4e12f6ef72b731d63fff8a17e4de288d5b
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-02-16 20:05:29 +0300

    add initPanel()

--- src/main/java/org/example/JavaSweeper.java
+++ src/main/java/org/example/JavaSweeper.java
@@ -6,22 +6,35 @@ import java.awt.*;
 /**
  * JFrame:
  * setVisible(true) - makes window visible
+ * add() - appends the specified component to the end of this container
  * pack() - causes this Window to be sized to fit the preferred size and layouts of its subcomponents
  * setDefaultCloseOperation() - operation that will happen by default when the user
  * initiates a "close" on this frame
- *              WindowConstants:
- *                      DO_NOTHING_ON_CLOSE
- *                      HIDE_ON_CLOSE
- *                      DISPOSE_ON_CLOSE
- *                      EXIT_ON_CLOSE
+ * WindowConstants:
+ * DO_NOTHING_ON_CLOSE
+ * HIDE_ON_CLOSE
+ * DISPOSE_ON_CLOSE
+ * EXIT_ON_CLOSE
  * setLocationRelativeTo() - Sets the location of the window relative
  * to the specified component according to the following scenarios
+ * <p>
+ * JPanel:
+ * setPreferredSize(new Dimension())
  */
 
 public class JavaSweeper extends JFrame {
 
+    private JPanel panel;
+
     private JavaSweeper() throws HeadlessException {
+        initPanel();
         initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 500));
+        add(panel);
     }
 
     private void initFrame() {
